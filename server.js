cf8018f0f1578cb90bad75b295b242751c82fed8
Evan Arbour <evan.arbour@gmail.com>
2021-09-07 12:39:16 -0700

    started intial prompt via inquirer

--- server.js
+++ server.js
@@ -3,15 +3,64 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const consoleTable = require('console.table');
 
-const PORT = process.env.PORT || 3001;
-
 // create connection of database and server.js
 const db = mysql.createConnection(
     {
         host: 'localhost',
+        port: 3001,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME
     },
     console.log('Connected to the company_db database.')
 );
+
+// initial prompt 
+function startPrompt() {
+    inquirer.prompt([
+    {
+        type: 'list', 
+        message: 'What would you like to do?',
+        name: 'initial',
+        choices: [
+            'View All Employees', 
+            'Add Employee', 
+            'Update Employee Role', 
+            'View All Roles', 
+            'Add Role', 
+            'View All Departments', 
+            'Add Department'
+        ]
+    }]).then((val) => {
+        switch (val.initial) {
+            case 'View All Employees':
+                viewAllEmployees();
+            break;
+
+            case 'Add Employee':
+                addEmployee();
+            break;
+
+            case 'Update Employee Role':
+                updateEmployeeRole();
+            break;
+
+            case 'View All Roles':
+                viewAllRoles();
+            break;
+
+            case 'Add Role':
+                addRole();
+            break;
+
+            case 'View All Departments':
+                viewAllDepartments();
+            break;
+
+            case 'Add Department':
+                addDepartment();
+            break;
+        };
+    });
+};
+
